0ed0b306e6e7832838e15418e3c9f48153740731
Camellia117 <embrace_death@163.com>
2025-02-17 10:47:55 +0800

    add some autorun programs

--- autorun.sh
+++ autorun.sh
@@ -6,21 +6,13 @@
 # awful.spawn.with_shell("xrandr --output eDP --mode 1920x1200 --rate 120")
 
 xrandr --output eDP --mode 1920x1200 --rate 120
-# volumeicon &
-# nm-applet &
-# blueman-applet &
-# flameshot &
-# fcitx5 &
 
-# 定义服务和启动命令的数组
 services=(
     "fcitx5:fcitx5"
     "nm-applet:nm-applet"
     "volumeicon:volumeicon"
     "blueman-applet:blueman-applet"
     "flameshot:flameshot"
-    # 添加其他服务，格式为 "服务名:启动命令"
-    # "service_name:command_to_start_service"
 )
 
 # 遍历数组并启动未运行的服务
@@ -37,22 +29,3 @@ for item in "${services[@]}"; do
         echo "$service_name is already running"
     fi
 done
-
-
-
-
-# start_if_not_running() {
-#   local service_name = "$1"
-#   local exec_command = "$2"
-#
-#   if ! pgrep -x "$service_name" > /dev/null; then
-#     eval "exec_command &"
-#     echo "Started $service_name"
-#   else
-#     echo "$service_name is already running"
-#   fi
-# }
-#
-#
-#
-#
